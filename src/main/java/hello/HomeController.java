6a3965fea3cc160bed03c9059f17985f87e5d0b4
WDB40 <wbrown1640@gmail.com>
2018-08-31 15:51:47 -0500

    Added the HttpRequestTest test class.

--- src/main/java/hello/HomeController.java
+++ src/main/java/hello/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Changing this for some checks.";
+        return "Now my tests will pass.";
     }
 
 }
